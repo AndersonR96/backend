3825d2dd8df939c45a7b481e7151ede686b5de35
Anderson Rubiano <anderrubiano@outlook.com>
2022-07-07 23:58:16 -0500

    update project

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,11 @@ const router = Router()
 require('dotenv').config()
 
 router.get('/', async function(req, res) {
-    // res.send(`Corriendo en el entorno <h3>${process.env.ENTORNO}</h3>`)
-    res.json({APP: process.env.APP, ENTORNO: process.env.ENTORNO, Puerto: process.env.PORT ? process.env.PORT : 4000})
+    res.json({
+        APP: process.env.APP, 
+        ENTORNO: process.env.ENTORNO, 
+        PUERTO: process.env.PORT ? process.env.PORT : 4000
+    })
 })
 
 module.exports = router
