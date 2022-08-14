5acf3791a3dd07e6f1abdca2e8e22865e74e009c
Miguel-Mendez-H <luis.miguel.mendez@hotmail.com>
2022-08-14 11:08:01 -0500

    se elimina purchase mientras

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,6 @@ import Provider from "./provider.js"
 import User from "./user.js"
 import Cost from "./cost.js"
 import Item from "./item.js"
-import Purchase from "./purchase.js"
 
 export const Controllers ={
     Customer,
@@ -11,5 +10,4 @@ export const Controllers ={
     User,
     Cost,
     Item,
-    Purchase,
 }
